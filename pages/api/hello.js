ba04073461c23f41737f73d141a57f7165ac8387
YashMhatre <yashmhatre62@gmail.com>
2021-01-11 18:48:15 +0530

    Publish blog is live with database write access and minor fixes to the editor

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  console.log(req.body) // The request body
+  console.log(req.query) // The url query string
+  console.log(req.method)
+  res.statusCode = 200;
+  res.json({ name: 'John Doe' });
 }
