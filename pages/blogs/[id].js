41ee1bac5c73bbc27813d0e0c6c3268d582f450b
YashMhatre <yashmhatre62@gmail.com>
2021-01-16 00:12:29 +0530

    set fallback to true. to allow newer urls to be shown

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -99,7 +99,7 @@ export async function getStaticPaths() {
 
     // We'll pre-render only these paths at build time.
     // { fallback: false } means other routes should 404.
-    return { paths, fallback: false }
+    return { paths, fallback: true }
 }
 
 
